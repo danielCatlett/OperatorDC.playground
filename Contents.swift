8cd4e0571ed63ef621216dc3ae859d73af9c7f25
danielCatlett <daniel.catlett42@gmail.com>
2017-02-03 10:05:06 -0600

    Defined some variables, and then did some math to them.

---
+++ Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+let w = 38
+let x = 40
+let y = 42
+let z = 44
+
+let a = 2
+let b = 4
+let c = 6
+let d = 8
+let e = 10.0
+let f = 0.2
+
+
+let h = w + a
+let i = x - b
+let j = y * c
+let k = z / d
+let l = e * f
+
+
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
